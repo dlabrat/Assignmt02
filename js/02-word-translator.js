c475110a666043969ecd18e16b7738297cda8010
dlabrat <107654952+dlabrat@users.noreply.github.com>
2026-02-25 13:26:46 -0800

    one left

--- js/02-word-translator.js
+++ js/02-word-translator.js
@@ -1,4 +1,16 @@
 //The Word Translator
 // Usage: Use simple conditional statements
 
+let langCode = prompt('Please enter your language code (es/de/en/fr): ')
 
+if (langCode === 'es') {
+    document.write('Hello World translated in Spanish is: Hola mundo')
+} else if (langCode === 'de') {
+    document.write('Hello World translated in German is: Hallo welt')
+} else if (langCode === 'en') {
+    document.write('Hello World in English is: Hello World')
+} else if (langCode === 'fr') {
+    document.write('Hello World translated in French is: Bonjour le monde')
+} else {
+    document.write('Hello World')
+}
--- js/03-coinflip.js
+++ js/03-coinflip.js
@@ -1,3 +1,24 @@
 //The “Coin Flip” Game
 //Usage: Use nested conditional statements
 
+let randomNum = Math.round(Math.random())
+let coinFlip = randomNum
+let choice = prompt('Plesae select Heads or Tails (enter h or t): ')
+
+//document.write(coinFlip)
+
+if (choice === 'h') {
+    if (coinFlip > 0) {
+        alert('The flip was heads and you chose heads...you win!')
+    } else if (coinFlip  <= 0) {
+        alert('The flip was tails but you chose heads...you lose!')
+    }
+}else if (choice === 't') {
+     if (coinFlip < 0) {
+        alert('The flip was tails and you chose tails...you win!')
+    } else if (coinFlip  >= 0) {
+        alert('The flip was heads but you chose tails...you lose!')
+    }
+} else {
+    alert('Thanks for playing.')
+}
