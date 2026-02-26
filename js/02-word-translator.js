95a68c5816856e0adf235ff7ef0799529a0180d7
dlabrat <107654952+dlabrat@users.noreply.github.com>
2026-02-25 16:15:22 -0800

    fisnished

--- js/02-word-translator.js
+++ js/02-word-translator.js
@@ -4,13 +4,13 @@
 let langCode = prompt('Please enter your language code (es/de/en/fr): ')
 
 if (langCode === 'es') {
-    document.write('Hello World translated in Spanish is: Hola mundo')
+    console.log('Hello World translated in Spanish is: Hola mundo')
 } else if (langCode === 'de') {
-    document.write('Hello World translated in German is: Hallo welt')
+    console.log('Hello World translated in German is: Hallo welt')
 } else if (langCode === 'en') {
-    document.write('Hello World in English is: Hello World')
+    console.log('Hello World in English is: Hello World')
 } else if (langCode === 'fr') {
-    document.write('Hello World translated in French is: Bonjour le monde')
+    console.log('Hello World translated in French is: Bonjour le monde')
 } else {
-    document.write('Hello World')
+    console.log('Hello World')
 }
--- js/04-grade-assigner.js
+++ js/04-grade-assigner.js
@@ -1,4 +1,22 @@
 //The “Grade Assigner” Application
 //Usage: Use else if or switch statements
 
-
+let grade =parseInt(prompt('Enter the student\'s grade: '))
+if ((grade<0)||(grade>100)) {
+    console.log('Only numbers 0 to 100 are accepted.')
+} 
+else if (grade >= 90) {
+    console.log('You received an A')
+}
+else if (grade >= 80) {
+    console.log('You received an B')
+}
+else if (grade >= 70) {
+    console.log('You received an C')
+}
+else if (grade>=60) {
+    console.log('You received an D')    
+}
+else {
+    console.log('You received an F')
+}
